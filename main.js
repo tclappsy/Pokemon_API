9e1cf88858ee759a93f588571b4474e3d348bed6
Thomas Clappsy <s1218375@monmouth.edu>
2021-01-18 17:06:04 -0500

    commit

---
+++ main.js
@@ -0,0 +1,42 @@
+
+const pokedex = document.getElementById("pokedex");
+
+
+const fetchPokemon = () => {
+const promises = [];
+
+    for(let i =1; i<= 150; i++) {
+
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    promises.push(fetch(url).then((res) => res.json()));
+    }
+
+    Promise.all(promises).then(results => {
+        const pokemon = results.map((data) => ({
+            name : data.name,
+            id: data.id,
+            image: data.sprites['front_default'],
+            type: data.types.map(type => type.type.name).join(', ')
+        }));
+
+        displayPokemon(pokemon);
+    });
+};
+
+const displayPokemon = (pokemon) => {
+    console.log(pokemon);
+
+    const pokemonHTMLString = pokemon.map((poke) => `
+    <li class = "card">
+        <img class = "card_image" src = "${poke.image}"/>
+        <h2 class = "card_title">${poke.id}. ${poke.name}</h2>
+        <p class = "card_p"> Type: ${poke.type}</p>
+    </li>
+    `
+    ).join("");;
+
+    pokedex.innerHTML = pokemonHTMLString;
+};
+
+
+fetchPokemon();
